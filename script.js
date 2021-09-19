2dc641115b80d09b21d983616d027b260a3bf18c
moredifferent <man20055@byui.edu>
2021-09-18 21:30:05 -0700

    the thing of stuff

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-function modate () {
-    let lmdate = document.lastModified();
-    document.getElementById("datelm").innerHTML = lmdate;
-}
-function copyr () {
-    let cdate = new Date();
-    document.getElementById("cyear").innerHTML = cdate.getFullYear();
-}
+document.getElementById("datelm").innerHTML = document.lastModified;
+const d = new Date();
+document.getElementById("cyear").innerHTML = d.getFullYear();
