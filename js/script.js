b008d7d083bc459ba1276705421f8fcdfb96d93d
moredifferent <man20055@byui.edu>
2021-10-09 20:17:06 -0700

    the thing of stuff

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const d = new Date();
-const year = d.getFullYear();
-const fulldate = `${year}`;
-document.querySelector("#cyear").textContent = fulldate;
 
-let lmdate = new Date(document.lastModified)
-document.querySelector("#datelm").textContent = lmdate;
+  document.getElementById("cyear").textContent = d.getFullYear();
+
+  let oLastModif = new Date(document.lastModified);
+  document.getElementById("datelm").textContent = oLastModif;
