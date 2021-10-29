4f03949eb3e47cb5ffb6926ac30cf5f90c8f931b
moredifferent <man20055@byui.edu>
2021-10-28 23:34:26 -0700

    the thing of stuff

--- lesson7/script/layzlazy.js
+++ lesson7/script/layzlazy.js
@@ -10,6 +10,11 @@ imagesToLoad.forEach((img) => {
   loadImages(img);
 });
 
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 200px 0px"
+};
+
 if('IntersectionObserver' in window) {
   const observer = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
@@ -18,7 +23,8 @@ if('IntersectionObserver' in window) {
         observer.unobserve(item.target);
       }
     });
-  });
+  }, imgOptions);
+
   imagesToLoad.forEach((img) => {
     observer.observe(img);
   });
